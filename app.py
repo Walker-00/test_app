9994c1e596b4994d3fac610d785361c1f58b05c1
Walker-00 <85013114+Walker-00@users.noreply.github.com>
2022-02-09 15:01:44 +0630

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def main():
     st.title("Test Edge detect Demo Project")
     st.sidebar.title("Test Edge detect Demo Project")
     st.sidebar.markdown("##Default Kernal size is 5 by 5")
-    ks = st.sidebar.selectbox("Kernal size for Gaussian", ("5 by 5", "3 by 3", "Default"))
+    ks = st.sidebar.selectbox("Kernal size for Gaussian", ("5 by 5", "7 by 3", "Default"))
     st.sidebar.markdown("##Recommend is 0")
     sx = st.sidebar.slider('Pick sigmaX value for Gaussian', 0, 10)
     st.sidebar.markdown("Recommend is 0")
